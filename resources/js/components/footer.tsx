925acc52b5d37532a2452994e27ab171f0293818
Dudás Tamás <ddstms@gmail.com>
2026-01-21 13:48:29 +0100

    feat: enhance footer component with navigation links and contact information

--- resources/js/components/footer.tsx
+++ resources/js/components/footer.tsx
@@ -1,7 +1,19 @@
+import { Link } from '@inertiajs/react';
 export default function Footer() {
  return (
   <div className="border border-t-chart-4">
-   <div className="px-2 py-6 md:px-12">footer</div>
+   <div className="flex flex-col items-center justify-evenly gap-6 px-2 py-6 text-center md:flex-row md:px-12 md:text-left">
+    <div className="flex flex-col gap-2 md:pt-4">
+     <Link className="text-lg">Ingatlanjaink</Link>
+     <Link className="text-lg">Kapcsolat</Link>
+     <Link className="text-lg">Adatvédelmi tájékoztató</Link>
+    </div>
+    <div className="flex flex-col gap-4">
+     <h3 className="text-2xl">Elérhetőségek:</h3>
+     <p>Tel: +36 30 549-6209</p>
+     <p>Email: ingatlanvonal@gmial.com</p>
+    </div>
+   </div>
   </div>
  );
 }
